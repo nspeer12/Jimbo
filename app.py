82c503691bdcb3968affe2d08eb607b8a833a55c
nspeer12 <nspeer252@gmail.com>
2020-09-02 09:02:04 -0400

    urban dictionary:

--- app.py
+++ app.py
@@ -1,60 +1,58 @@
 import markdown
 from flask import Flask, render_template, request, jsonify, make_response
-from flask_assistant import Assistant, ask
 import markdown.extensions.fenced_code
 from pygments.formatters import HtmlFormatter
 import random
 import json
+import requests
+import re
+
 
 app = Flask(__name__)
 app.static_folder = "static"
-
-# initialize Assistant
-assistant = Assistant(app, "/google")
 
 @app.route("/")
 def index():
     return render_template("index.html")
 
-@app.route("/mdtest")
-def markdown_render():
-    readme_file = open("README.md", "r")
-    md_template_string = markdown.markdown(
-                         readme_file.read(), extensions=["fenced_code"])
-
-    formatter = HtmlFormatter(style="emacs", full=True,cssclass="codehilite")
-    css_string = formatter.get_style_defs()
-    md_css_string = "<style>" + css_string + "</style>"
-
-    return md_css_string + md_template_string
-
-
-@app.route("/random", methods=["GET", "POST"])
+@app.route("/urban", methods=["GET", "POST"])
 def webhook():
     # parse data from request
     if request.method == "POST":
         req = request.get_json(silent=True, force=True)
         params = req["queryResult"]["parameters"]
-        if "min" in :
-            if params["min"] is not '':
-                print(params)
-                params
-                minNum = params["min"]
-                maxNum = params["max"]
-                res = get_random_number(minNum=minNum, maxNum=maxNum)
-            else:
-                print(params)
-                res = get_random_number()
+        if "phrase" in params and params["phrase"] is not None:
+            phrase = params["phrase"]
+            print(phrase)
+            res = define(phrase)
 
     res = json.dumps(res, indent=4)
     r = make_response(res)
     r.headers["Content-Type"]="application/json"
     return r
 
+def define(phrase):
+    url = "https://mashape-community-urban-dictionary.p.rapidapi.com/define"
 
-def get_random_number(minNum=0, maxNum=100):
-    randomNum = random.randint(minNum, maxNum)
-    return fulfillment_message(str(randomNum))
+    querystring = {"term": phrase}
+
+    headers = {
+        "x-rapidapi-host": "mashape-community-urban-dictionary.p.rapidapi.com",
+        "x-rapidapi-key": "a9520b2012mshe2a098d392901cfp18b21ejsn7cd5055df26e"
+        }
+
+    response = requests.request("GET", url, headers=headers, params=querystring)
+    definition = json.loads(response.text)
+
+    # parse definition from json
+    if "definition" in definition["list"][0]:
+        definition = definition["list"][0]["definition"]
+        definition = definition.replace("[", "")
+        definition = definition.replace("]", "")
+        print(definition)
+        return fulfillment_message(definition)
+    else:
+        return fulfillment_message("no definition was found")
 
 def fulfillment_message(text):
     return {
