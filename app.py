4bd07eede9a6e1623ce22f4589520275aa0af2ef
nspeer12 <nspeer252@gmail.com>
2020-10-11 15:33:07 +0100

    init

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ import random
 import json
 import requests
 import re
+import time
 
 
 app = Flask(__name__)
@@ -18,10 +19,10 @@ def index():
 
 @app.route("/privacy-policy")
 def privacy():
-    return render_template("privacy_policy.html")
+    return render_template("privacy-policy.html")
 
 
-@app.route("/urban", methods=["GET", "POST"])
+@app.route("/webhook", methods=["GET", "POST"])
 def webhook():
     # parse data from request
     if request.method == "POST":
@@ -30,24 +31,32 @@ def webhook():
 
         req = request.get_json(silent=True, force=True)
 
-        f = open("request.txt", "w")
+        f = open("logs/" + str(time.time()) + ".txt", "w")
         f.write(json.dumps(req, indent=4, sort_keys=True))
         f.close()
+        
         # get intent 
         intent = req["queryResult"]["intent"]["displayName"]
         print(intent)
         params = req["queryResult"]["parameters"]
 
-        if intent == "define":
+        if "define" in intent:
             if "phrase" in params and params["phrase"] is not None:
                 phrase = params["phrase"]
-                #print(phrase)
                 res = define(phrase)
-            
                 res = assistant_response(res)
                 res = json.dumps(res, indent=4)
                 r = make_response(res)
                 r.headers["Content-Type"]="application/json"
+                return r
+        elif "joke" in intent:
+            with open("jokes/nsfw.json") as f:
+                data = json.load(f)
+                joke = random.choice(data["jokes"])
+                res = assistant_response(joke)
+                res = json.dumps(res, indent=4)
+                r = make_response(res)
+                r.headers["Content-Type"] = "application/json"
                 return r
 
 
@@ -65,12 +74,16 @@ def define(phrase):
     definition = json.loads(response.text)
 
     # parse definition from json
-    if "definition" in definition["list"][0]:
-        definition = definition["list"][0]["definition"]
-        definition = definition.replace("[", "")
-        definition = definition.replace("]", "")
-        print(definition)
-        return definition
+    if len(definition["list"]) > 0:
+        if "definition" in definition["list"][0]:
+            definition = definition["list"][0]["definition"]
+            definition = definition.replace("[", "")
+            definition = definition.replace("]", "")
+           
+            print(definition)
+            return definition
+    else:
+        return "Oh shit, something went wrong. Try again in a few minutes"
 
 
 def fulfillment_message(text):
@@ -87,11 +100,11 @@ def fulfillment_message(text):
     }
 
 
-def assistant_response(text):
+def assistant_response(text, response=False):
     return {
             "payload": {
                 "google": {
-                    "expectUserResponse": False,
+                    "expectUserResponse": response,
                     "richResponse": {
                         "items": [
                             {
@@ -104,6 +117,7 @@ def assistant_response(text):
                 }
             }
         }
+
 
 
 if  __name__ == "__main__":
